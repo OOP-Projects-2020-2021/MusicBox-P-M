a2d71fe01e2ceb3262b63a776ef0cc5fdeef40fa
Moisa Oana Miruna <oana_moisa29@yahoo.com>
2020-12-19 20:07:19 +0200

    added ArtistFeed class

---
+++ src/com/company/ArtistFeed.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class ArtistFeed {
+    private ArrayList<String> post=new ArrayList<>();
+
+    public ArrayList<String> getPost() {
+        return post;
+    }
+    public void setPost(ArrayList<String> post) {
+        this.post = post;
+    }
+}
--- src/com/company/RegularUser.java
+++ src/com/company/RegularUser.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class RegularUser extends User{
-    Set<Artist> listOfArtists = new HashSet<Artist>();
+    private Set<Artist> listOfArtists = new HashSet<Artist>();
     public RegularUser(String username,String password, String name, String imagePath)
     {
         super(username,password,name,imagePath);
@@ -13,7 +13,6 @@ public class RegularUser extends User{
     public Set<Artist> getListOfArtists() {
         return listOfArtists;
     }
-
     public void setListOfArtists(Set<Artist> listOfArtists) {
         this.listOfArtists = listOfArtists;
     }
